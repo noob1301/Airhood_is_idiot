3bbb4687c780395b07084317ec31cee363a76920
noob1301 <160331480+noob1301@users.noreply.github.com>
2024-08-14 14:10:27 +0900

    업데이트

---
+++ codeup1601-1700/1630.cpp
@@ -0,0 +1,4 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(int n){cin>>n;string a[n];for(int i=0;i<n;i++)cin>>a[i];for(int i=0;i<n-1;i++)cout<<a[i]<<endl<<"AMOLED"<<endl;cout<<a[n-1];}
