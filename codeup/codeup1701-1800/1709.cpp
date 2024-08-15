030885e910e381362d1fffd25afeee1ee939cef3
noob1301 <160331480+noob1301@users.noreply.github.com>
2024-08-16 01:21:57 +0900

    codeup1701-1800 업데이트

---
+++ codeup/codeup1701-1800/1709.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    for(int i=0;i<n-1;i++)
+        for(int j=0;j<n-i-1;j++)
+            if(a[j]<a[j+1]){
+                int b=a[j];
+                a[j]=a[j+1];
+                a[j+1]=b;
+            }
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<' ';         
+}
