606b5deeedded79f27e7cea3587167dad553dae1
noob1301 <160331480+noob1301@users.noreply.github.com>
2024-08-17 00:04:10 +0900

    1917.cpp 업데이트

--- codeup/codeup1901-2000/1917.cpp
+++ codeup/codeup1901-2000/1917.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int solution(int a,int b) {
+long long intsolution(int a,int b) {
     if(b==1)
         return a;
     return a*solution(a,b-1);    
@@ -11,7 +11,7 @@ int main() {
     if(k==0||n==1)
         cout<<1;
     else if(n==-1)
-        cout<<(k%2==0)?1:-1;
+        cout<<((k%2==0)?1:-1);
     else if(n==0)
         cout<<0;    
     else
