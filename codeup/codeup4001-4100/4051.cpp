d744aba03ee150f3d196b5838966548ba3061388
noob1301 <160331480+noob1301@users.noreply.github.com>
2024-09-25 10:39:50 +0900

    codeup4001-4100 업데이트

---
+++ codeup/codeup4001-4100/4051.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stack>
+int main(){
+    float s,e,S=0;
+    for(int i=0;i<5;i++){
+        scanf("%f%f",&s,&e);
+        if(e-s<1)
+            continue;
+        else
+            S+=(e-s>=5)?4:e-s-1;    
+    }
+    if(S>=15||S<=5)
+        printf("%g",(S>=15)?0.95*S*10000:1.05*S*10000);
+    else
+        printf("%g",S*10000);    
+}
