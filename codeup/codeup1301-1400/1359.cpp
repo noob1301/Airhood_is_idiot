1586f81474fb799eba30fe477d2dc5708ef7c031
noob1301 <160331480+noob1301@users.noreply.github.com>
2024-10-01 00:55:39 +0900

    codeup1301-1400 업데이트

---
+++ codeup/codeup1301-1400/1359.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++)
+            printf("%d ",j+1);
+        printf("\n");
+    }
+}
