8728d9a812bb535bd495c5163df0b70b83a6a23f
noob1301 <160331480+noob1301@users.noreply.github.com>
2024-12-10 14:47:43 +0900

    codeup1101-1200 업데이트

---
+++ codeup/codeup1101-1200/1172.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<algorithm>
+int main(){
+    int a[3];
+    for(int i=0;i<3;i++) scanf("%d",&a[i]);
+    std::sort(a,a+3);
+    for(int i=0;i<3;i++) printf("%d ",a[i]);
+}
