77d917eb64a67bd2c42020b87a648e21b9058235
noob1301 <160331480+noob1301@users.noreply.github.com>
2025-05-20 13:28:10 +0900

    Create 1507.cpp

---
+++ codeup/codeup1501-1600/1507.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+int main(){
+    int l[100][100]={0,},x1,y1,x2,y2,s=0;
+    for(int i=0;i<4;i++){
+        scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+        for(int j=x1;j<x2;j++)
+            for(int k=y1;k<y2;k++)
+                l[j][k]=1;
+}
+    for(int i=0;i<100;i++){
+        for(int j=0;j<100;j++){
+            if(l[i][j]==1) s++;
+        }
+    }
+    printf("%d",s);
+}
